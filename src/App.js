60d2442925f8f8737b07307ee33ae3614d10a57c
amritanshm21 <47923152+amritanshm21@users.noreply.github.com>
2021-01-22 17:46:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,246 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="frame">
+          <div className="frame1">
+            <div id="fixedFrame">
+              <div className='box3'>
+                  <img src={require('./guy.jpg').default} className="img1" alt="profilePic" />
+                  <p className="inpt"></p>
+                  <img src={require('./message.png').default} className="icon2" alt="profilePic" />
+                  <img src={require('./three-dot.png').default} className="icon2" alt="profilePic" />
+                </div>
+                <div className='box2'>
+                  <img src={require('./search.png').default} className="img3" alt="profilePic" />
+                  <input type='text' className="input3" placeholder='Search or start new chat' />
+                </div>
+              <div id='frameMain'>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">Daniel</div>
+                        <div class='inputTwo'>9:45 AM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>how are you</div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic" /> </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">Joker</div>
+                        <div class='inputTwo'>11:10 PM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>hello</div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic" /> </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">John</div>
+                        <div class='inputTwo'>9:45 PM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>hey man</div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic" /> </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">Doe</div>
+                        <div class='inputTwo'>10:05 PM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>yeah boy</div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic" /> </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">Michael</div>
+                        <div class='inputTwo'>8:07 PM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>Thanks Buddy</div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic" /> </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">Clark</div>
+                        <div class='inputTwo'>6:31 PM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>Quite good</div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic" /> </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">kent</div>
+                        <div class='inputTwo'>5:10 PM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>So how's going?</div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic"/> </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">Daniel</div>
+                        <div class='inputTwo'>9:45 AM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>how are you</div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic" /> </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">Dextro</div>
+                        <div class='inputTwo'>4:15 PM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>Feeling good </div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic" /> </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">Flash</div>
+                        <div class='inputTwo'>9:45 PM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>how are you</div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic" /> </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">Storm</div>
+                        <div class='inputTwo'>9:45 PM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>how are you</div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic" /> </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">Shadow</div>
+                        <div class='inputTwo'>9:45 PM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>how are you</div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic" /> </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='box1'>
+                  <div className='main1'>
+                  <img src={require('./a1.png').default} className="img1" alt="profilePic" />
+                  </div>
+                  <div id='main2'>
+                    <div id="two">
+                        <div class="inputOne">Sweety 😍😍</div>
+                        <div class='inputTwo'>9:45 PM</div>
+                    </div>
+                    <div id="three">
+                        <div class='inputThree'>how are you</div>
+                        <div class='inputTwo'><img src={require('./mute.jpg').default} className="icon1" alt="profilePic" /> </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div className="frame2">
+              <div className='para1'>
+                <div className='title1'>
+                  <img src={require('./MDWHIZ LOGO_02.png').default} className="iconDp" alt="profilePic" />
+                  <div className="inpt">AMCI Pvt Ltd Group 😇</div>
+                </div>
+                <div className='title2'>
+                  <img src={require('./share.png').default}  alt="profilePic" className="icon2" />
+                  <img src={require('./three-dot.png').default} className="icon2" alt="profilePic" />
+                </div>
+              </div>
+              <div className='para2'>
+              </div>
+              <div className='para3'> 
+                <img src={require('./smiley.jpg').default} className="img2" alt="profilePic" />
+                <input type='text' className='input2' placeholder='Type anything' />
+                <img src={require('./mic.png').default} className="icon2" alt="profilePic" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
